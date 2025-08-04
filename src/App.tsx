71eed6d7d046200b1588b6eed7d928edfafcd92a
gontroller <gontrollerdev@gmail.com>
2025-08-04 20:40:55 -0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+<<<<<<< HEAD
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
 import Benefits from './components/Benefits/Benefits';
@@ -16,6 +17,15 @@ function App() {
       </main>
     </div>
   );
+=======
+
+function App() {
+  return (
+    <div>
+      <h1>Skin Savvy</h1>
+    </div>
+  )
+>>>>>>> d6950d7 (REACTDOM)
 }
 
 export default App;
