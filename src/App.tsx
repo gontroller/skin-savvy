059cf382d23fc5041cadd75a66acbfad851d0e93
gontroller <gontrollerdev@gmail.com>
2025-08-05 11:57:57 -0300

    finalizacoes

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Header from './components/Header/header';
+import Header from './components/Header/Header.tsx';
 import Hero from './components/Hero/Hero';
 import Benefits from './components/Benefits/Benefits';
 import Products from './components/Products/Products';
